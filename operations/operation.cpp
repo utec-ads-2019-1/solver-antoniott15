ded9229e1726d5be31fddb474e0af2a2abe461bc
Antonio <antonio.toche@utec.edu.pe>
2019-05-18 21:37:45 -0500

    parenthesis

--- operations/operation.cpp
+++ operations/operation.cpp
@@ -2,10 +2,25 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+void getParenthesis(string equation, char digits, int &position)
+{
+    int parenthesis = 0;
+    while (position != 0)
+    {
+        position--;
+        if (equation[position] == '(')
+        {
+            parenthesis++;
+        }
+        if (equation[position] == ')')
+        {
+            parenthesis--;
+        }
+    }
+}
 
 Operation *Operation::buildFromEquation(string equation)
 {
-    cout << equation << endl;
 
     return 0;
 }
