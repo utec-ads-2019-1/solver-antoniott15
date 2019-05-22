718e19d0d04218c8fd85cf638a7aa12dc856738a
Antonio <antonio.toche@utec.edu.pe>
2019-05-22 14:53:28 -0500

    doesnt work

--- operations/operation.cpp
+++ operations/operation.cpp
@@ -1,26 +1,49 @@
 #include "operation.h"
 #include <iostream>
+#include <string>
 #include <vector>
 using namespace std;
-void getParenthesis(string equation, char digits, int &position)
+
+bool Digit(string equation)
 {
-    int parenthesis = 0;
-    while (position != 0)
+    for (int i = 0; i < equation.length(); i++)
     {
-        position--;
-        if (equation[position] == '(')
-        {
-            parenthesis++;
-        }
-        if (equation[position] == ')')
-        {
-            parenthesis--;
-        }
+        if (!(equation[i] >= '0' && equation[i] <= '9'))
+            return false;
+    }
+    return true;
+}
+
+Operation *buildFromEquation(string equation)
+{
+    int position = equation.length() - 1;
+
+    int newPositionPow = equation.find('^');
+    int newPositionMult = equation.find('*');
+    int newPositionDiv = equation.find('/');
+    int newPositionSubs = equation.find('-');
+    int newPositionPlus = equation.find('+');
+    if (equation[newPositionPow] != -1)
+    {
+        cout << equation[newPositionPow] << endl;
+
+        string rightExpression = equation.substr(0, position);
+        string leftExpression = equation.substr(position);
+        cout << rightExpression << endl;
+        cout << leftExpression << endl;
+        return new powNumber(buildFromEquation(rightExpression), buildFromEquation(leftExpression));
+    }
+    else if (Digit(equation))
+    {
+        float numbers = strtof(equation.c_str(), 0);
+        return new factorEx(numbers);
     }
 }
 
-Operation *Operation::buildFromEquation(string equation)
+int main()
 {
-
+    string expression = "4^2";
+    Operation *equation = buildFromEquation(expression);
+    cout << equation->operate() << endl;
     return 0;
 }
--- operations/operation.h
+++ operations/operation.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <math.h>
+
 using namespace std;
 
 class Operation
@@ -13,82 +14,54 @@ protected:
 public:
     Operation *left;
     Operation *right;
-    int data;
+    float data;
     Operation() : left(nullptr), right(nullptr){};
     Operation(Operation *left, Operation *right) : left(left), right(right){};
-    Operation(int result) : data(result){};
+    Operation(float result) : data(result){};
     static Operation *buildFromEquation(string equation);
-    virtual float operate() = 0;
+    static Operation *finding(char expression, string equation, int &position);
+    virtual float operate() { return 0; };
 };
-
-class digit : public Operation
+class factorEx : public Operation
 {
 public:
-    digit(int number) : Operation(number) {}
-    float operate();
+    factorEx(float number) : Operation(number) {}
+    float operate() { return data; };
 };
-float digit::operate()
-{
-    return data;
-}
 
 class adition : public Operation
 {
 public:
     adition(Operation *left_, Operation *right_) : Operation(left_, right_){};
-    float operate();
+    float operate() { return left->operate() + right->operate(); };
 };
-float adition::operate()
-{
-    Operation::operate();
-    return left->operate() + right->operate();
-}
 
 class substraction : public Operation
 {
 public:
     substraction(Operation *left_, Operation *right_) : Operation(left_, right_){};
-    float operate();
+    float operate() { return left->operate() - right->operate(); };
 };
-float substraction::operate()
-{
-    Operation::operate();
-    return left->operate() - right->operate();
-}
-
-class multiply : public Operation
-{
-public:
-    multiply(Operation *left_, Operation *right_) : Operation(left_, right_){};
-    float operate();
-};
-float multiply::operate()
-{
-    Operation::operate();
-    return left->operate() * right->operate();
-}
 
 class division : public Operation
 {
 public:
     division(Operation *left_, Operation *right_) : Operation(left_, right_){};
-    float operate();
+    float operate() { return left->operate() / right->operate(); };
 };
-float division::operate()
+
+class multiply : public Operation
 {
-    Operation::operate();
-    return left->operate() / right->operate();
-}
+public:
+    multiply(Operation *left_, Operation *right_) : Operation(left_, right_){};
+    float operate() { return left->operate() * right->operate(); };
+};
 
 class powNumber : public Operation
 {
 public:
     powNumber(Operation *left_, Operation *right_) : Operation(left_, right_){};
-    float operate();
+    float operate() { return pow(left->operate(), right->operate()); };
 };
-float powNumber::operate()
-{
-    Operation::operate();
-    return pow(left->operate(), right->operate());
-}
+
 #endif
